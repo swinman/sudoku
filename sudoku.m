af913c1c825ed58805d647d886ecc2af02446c97
Walker Inman <winman@lucidsci.com>
2018-02-20 21:28:51 -0500

    remove old matlab code that has been incorporated

--- sudoku.m
+++ sudoku.m
@@ -1,26 +1,49 @@
-function [X,P]=sudoku(X)
-clc
-
-P=initialize(X);
-RO_old=9^3;
-RO=sum(sum(sum(P,3)-1+(sum(P,3)~=1)));
-%RB=sum(sum(sum(P,3)~=1));
-while (RO>0 && RO~=RO_old)
-  RO_old=RO;
-  [X,P]=find_elim(X,P);   % uses possibility array to update X
-  P=comp_sets(P);         % updates possibility array
-  P=num_forcing(P);       % updates possibility array
-  RO=sum(sum(sum(P,3)-1+(sum(P,3)~=1)));
-end
-X=find_elim(X,P);         % fills in X if need be
-
-RB=sum(sum(sum(P,3)~=1));
-RO=sum(sum(sum(P,3)-1+(sum(P,3)~=1)));
-if RB==0
-  clc, disp('The solution is:'), disp(X)
-else
-  disp('.'), disp_cur(P,X);
-  fprintf('%0.0f Remaining Boxes\n',RB)
-  fprintf('%0.0f Remaining Possibilities\n',RO)
-  check_ok(P);            % checks that solution can exist
-end
+function X = sudoku_1(X) 
+% SUDOKU  Solve Sudoku using recursive backtracking. 
+%   sudoku(X), expects a 9-by-9 array X. 
+ % Fill in all “singletons”. 
+ % C is a cell array of candidate vectors for each cell. 
+ % s is the first cell, if any, with one candidate. 
+ % e is the first cell, if any, with no candidates. 
+ [C,s,e] = candidates(X); 
+ while ~isempty(s) && isempty(e) 
+    X(s) = C{s}; 
+    [C,s,e] = candidates(X); 
+ end 
+ % Return for impossible puzzles. 
+ if ~isempty(e) 
+    return 
+ end 
+ % Recursive backtracking. 
+ if any(X(:) == 0) 
+    Y = X; 
+    z = find(X(:) == 0,1);    % The first unfilled cell. 
+    for r = [C{z}]            % Iterate over candidates. 
+       X = Y; 
+       X(z) = r;              % Insert a tentative value. 
+       X = sudoku(X);         % Recursive call. 
+       if all(X(:) > 0)       % Found a solution. 
+          return 
+       end 
+    end 
+ end
+% ------------------------------ 
+   function [C,s,e] = candidates(X) 
+      C = cell(9,9); 
+      tri = @(k) 3*ceil(k/3-1) + (1:3); 
+      for j = 1:9 
+         for i = 1:9 
+            if X(i,j)==0 
+               z = 1:9; 
+               z(nonzeros(X(i,:))) = 0; 
+               z(nonzeros(X(:,j))) = 0; 
+               z(nonzeros(X(tri(i),tri(j)))) = 0; 
+               C{i,j} = nonzeros(z)'; 
+            end 
+         end 
+      end 
+ L = cellfun(@length,C);   % Number of candidates. 
+ s = find(X==0 & L==1,1); 
+ e = find(X==0 & L==0,1); 
+ end % candidates 
+end % sudoku
